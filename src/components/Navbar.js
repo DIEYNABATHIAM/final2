5b59155a616041940a353ec7a398418e3d9e73a7
DIEYNABATHIAM <dondoudieynabathiam@gmail.com>
2023-07-26 19:04:17 +0200

    free

---
+++ src/components/Navbar.js
@@ -0,0 +1,177 @@
+import React from "react"; 
+import "@fortawesome/fontawesome-free/css/all.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from '../assets/gd.jpg';
+import Logo2 from '../assets/2.png';
+import Logo3 from '../assets/3.PNG';
+import Logo4 from '../assets/4.png';
+import Logo5 from '../assets/5.png';
+import Logo6 from '../assets/6.png';
+import Logo7 from '../assets/7.png';
+import Logo8 from '../assets/8.png';
+import Logo9 from '../assets/9.png';
+import Logo10 from '../assets/l.png';
+import Logo11 from '../assets/11.png';
+import Logo12 from '../assets/12.png';
+import Logo14 from '../assets/14.png';
+import Logo13 from '../assets/13.png';
+import Icon from '../assets/Icon.png';
+import Icon1 from '../assets/icon1.png';
+import Logo15 from '../assets/icon15.png';
+import Logo16 from '../assets/16.png';
+import Logo17 from '../assets/17.png';
+import Logo18 from '../assets/18.png';
+import Logo19 from '../assets/19.png';
+import '../components/Navbar.css';
+
+function Navbar() {
+  return (
+
+    <div className='navbar1'>
+    <div className='navbar'>
+    <div className='sid'>
+          <div className='zone'>
+     <strong>OZONE</strong>
+      </div>
+      <div className='side'>
+      <i class="fas fa-phone-alt">+15244455511</i>
+      </div>
+      <div className='titre4'>
+      <div className='leftside'>
+        <img src={Logo} class="float-right" />
+         </div>
+         <div className='titre0'>
+        <strong> <h2>CREATIVE DIGITAL<br></br> Marketing Agency</h2></strong>
+         <h4>React-Bootstrap remplace le JavaScript Bootstrap.<br></br> Chaque composant a été<br></br> construit à partir de zéro comme un véritable .</h4>
+         <button type="button" class="btn btn-success col-7">contact</button>
+         </div>
+         </div>  
+         </div>
+        
+         <div className=" fead">
+         <div className=" fead">
+                <h4><strong>Featured in </strong>|OurCertification</h4>
+                </div>
+                </div>
+                
+         <div class="box-pad">
+       <div class="box-un">
+       <img src={Logo10} class="float-right " />
+       
+   </div>    
+       <div class="box-un">
+       <img src={Logo11} class="float-right " />  
+       </div>
+       <div class="box-un">
+       <img src={Logo12} class="float-right " />  
+       </div>
+       <div class="box-un">
+       <img src={Logo14} class="float-right " /> 
+       </div>
+       <div class="box-un">
+       <img src={Logo13} class="float-right " /> </div>
+       </div>
+       </div>
+       <div class="box-pad2">
+        <div class="box-un2">
+        <img src={Icon} class="float-right " />
+        <strong><h5>Search <br></br>argine optimization</h5></strong>
+    </div>    
+        <div class="box-un2">
+        <img src={Icon1} class="float-right " />     
+        <strong> <h5>website design & <br></br> developpement</h5></strong> 
+        </div>   
+        <div class="box">
+        <strong> <h2>OUR Digital<br></br> Marketing Expertise</h2> </strong>
+        <h6>Headings must have content and the content<br></br> must be accessible by a screen reade 
+        <br></br>Headings must have content and the<br></br> content must be accessible by a screen reade</h6> 
+        </div>
+       </div>
+       <div class="box-pad3">
+       <div class="box-un3">
+       <img src={Logo16} class="float-right " />
+      <h5> video web editing<br></br> & productive</h5>  
+   </div>    
+       <div class="box-un3">
+       <img src={Logo17} class="float-right " />       
+       <h5>content <br></br>write</h5>  
+       </div>
+       <div class="box-un3">
+       <img src={Logo15} class="float-right " />    
+       <h5>social mediat<br></br> Marketing</h5>  
+       </div>
+       <div class="box-un3">
+       <img src={Logo18} class="float-right " />  
+       <h5>Pay Per <br></br> click (Ppc)</h5>  
+       </div>
+       </div>
+       <div className='back1'>
+       <div className='back'>
+       <div className='logo2'>
+        <img src={Logo2} class="float-right " />
+         </div>
+         <div className='blog'>
+         <img src={Logo19} class="float-right " height="100%"
+    width="100%"  />
+         </div>
+         </div>
+         </div>
+         
+         <div className='titre2'>
+         <h1><strong>WHY should you choose Ozone</strong></h1>
+         <h4>React-Bootstrap remplace le JavaScript Bootstrap.<br></br> Chaque composant a été<br></br> construit à partir de zéro comme un véritable <br></br> composant React, sans dépendances inutiles comme jQuery.</h4>
+         <button type="button" class="btn btn-primary">contact</button>
+         </div>
+         <div className="images">
+            <h3>Our latest work</h3>
+            <div className="img">
+            <img src={Logo3} class="float-right " />
+            <h4><strong>artem digital marketing <br></br> compaign</strong></h4>
+            <h5> img elements must have an alt prop, <br></br>either with meaningful either with meaningful </h5>
+            </div>
+            <div className="img">
+            <img src={Logo4} class="float-right " />
+            <h4><strong>mayhen-Search engine <br></br>optimization</strong></h4>
+            <h5> img elements must have an alt prop, <br></br>either with meaningful <br></br>either with meaningful </h5>
+            </div>
+            
+            <div className="img">
+            <img src={Logo5} class="float-right " />
+            <h4><strong>Basic - pay per click (PPP)</strong><br></br> compaign</h4>
+            <h5> img elements must have an alt prop, <br></br>either with meaningful either with meaningful </h5>
+            </div>
+            </div>
+            <div class="bord">
+                <h2>Testimonials</h2>
+        <div class="bord2">
+        <img src={Logo6} class="float-right " /><strong>Zeo Mantis</strong><br></br>Search argine optimization<br></br>
+       <h6>must have an alt prop, either <br></br>Search argine op with meaningful</h6>  
+    </div> 
+    <div class="bord2">
+    <img src={Logo7} class="float-right " /><strong>Don joe </strong><br></br>Search argine optimization<br></br>
+       <h6>must have an alt prop, either <br></br>Search argine op with meaningful</h6>  
+    </div>
+    <div class="bord2">
+    <img src={Logo8} class="float-right " /><strong>Natalis Colis</strong><br></br>Search argine optimization<br></br>
+       <h6>must have an alt prop, either <br></br>Search argine op with meaningful</h6>  
+    </div>   
+    </div>
+    <div className='tout'>
+    <div className='titre3'>
+         <h2>CREATIVE DiGITAL <br></br>Marketing Agency</h2>
+         <h5>React-Bootstrap remplace le JavaScript Bootstrap.<br></br> Chaque composant a été<br></br> construit à partir de zéro comme un véritable <br></br> composant React, sans dépendances inutiles comme jQuery.</h5>
+         <button type="button" class="btn btn-success btn-lg">contact</button> OR<strong> <h4>+7511466444</h4></strong>
+         <h4>Create by dieynabathiam</h4>
+         </div>
+    <div className='droite'>
+        <img src={Logo9} class="float-right " />
+        <h4>Create by dieynabathiam</h4>
+         </div>            
+       </div>
+       </div>
+      
+    
+  );
+}
+
+export default Navbar;
